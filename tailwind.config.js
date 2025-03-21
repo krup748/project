5f44bff467841cfaaad630e14d849ba26c1eb606
krup748 <sharmakrupa2413@gmail.com>
2025-03-21 10:26:58 +0530

    frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ export default {
   theme: {
     extend: {
       colors:{
-        'primary': "#5F6FFF"
+        'primary':"#5f6FFF"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill,minmax(200px,1fr))'
       }
     },
   },
   plugins: [],
 }
+
